b1b31ea5ac12848016305e60c224f6990db863fa
David Lam <dav_lam@ymail.com>
2016-12-08 19:19:20 -0500

    wip

--- homework02/src/alternate_history.py
+++ homework02/src/alternate_history.py
@@ -19,10 +19,22 @@ def parser():
     return arguments
 
 
+def git_commit_tree(tree, parent, message):
+    commit_tree = subprocess.run(["git", "commit-tree", tree, "-p", parent, "-m", message], stdout=subprocess.PIPE);
+    return commit_tree
+
+def generate_alternate_history(trees, branch_name):
+
+    # Source for fixed number format: https://stackoverflow.com/questions/17118071/python-add-leading-zeroes-using-str-format
+    message = "alternate_history{0:0>2}"
+    for k in xrange(0, -1, -1):
+        git_commit_tree(trees[k - 1], trees[k], message.format(k))
 
 def main():
     arguments = parser()
     print(arguments)
+
+
 
 if __name__ == "__main__":
     main()
